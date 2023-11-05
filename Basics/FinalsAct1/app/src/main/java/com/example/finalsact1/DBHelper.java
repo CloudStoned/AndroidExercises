dc6eb4654f1cc807a8b6ca9af2c52c6dd19cf0a1
CloudStone <92814911+CloudStoned@users.noreply.github.com>
2023-11-05 16:12:23 +0800

    Modified: Employee Activity

--- Basics/FinalsAct1/app/src/main/java/com/example/finalsact1/DBHelper.java
+++ Basics/FinalsAct1/app/src/main/java/com/example/finalsact1/DBHelper.java
@@ -15,23 +15,29 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase DB) {
-        DB.execSQL("CREATE TABLE StudentData(studentId TEXT primary key, studentName TEXT, studentCourse TEXT )");
+        DB.execSQL("CREATE TABLE EmployeesTable(employeeID TEXT primary key, employeeFullName TEXT, Gender TEXT, currentWeight TEXT, Height TEXT, goalWeight TEXT, Age TEXT, Phone TEXT, Address TEXT )");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase DB, int oldVersion, int newVersion) {
-        DB.execSQL("DROP TABLE if exists StudentData");
+        DB.execSQL("DROP TABLE if exists EmployeesTable");
     }
 
-    public Boolean InsertData (String studId, String studName, String studCourse)
+    public Boolean InsertData (String empId, String fullName,  String gender, String cWeight, String height, String gWeight, String age, String phone, String address)
     {
         SQLiteDatabase DB = this.getWritableDatabase();
         ContentValues contentV = new ContentValues();
-        contentV.put("studentId", studId);
-        contentV.put("studentName", studName);
-        contentV.put("studentCourse", studCourse);
+        contentV.put("employeeID", empId);
+        contentV.put("employeeFullName",fullName);
+        contentV.put("Gender",gender);
+        contentV.put("currentWeight",cWeight);
+        contentV.put("Height",height);
+        contentV.put("goalWeight",gWeight);
+        contentV.put("Age",age);
+        contentV.put("Phone",phone);
+        contentV.put("Address",address);
 
-        long result = DB.insert("StudentData",null,contentV);
+        long result = DB.insert("EmployeesTable",null,contentV);
 
         if(result == -1)
         {
@@ -43,35 +49,13 @@ public class DBHelper extends SQLiteOpenHelper {
         }
     }
 
-    public Boolean UpdateData (String studentId, String newStudentName, String newStudentCourse)
-    {
+    public Cursor SearchData(String employeeId) {
         SQLiteDatabase DB = this.getWritableDatabase();
-        ContentValues contentV = new ContentValues();
-        contentV.put("studentName", newStudentName);
-        contentV.put("studentCourse", newStudentCourse);
+        String[] columns = {"employeeID","employeeFullName","Gender","currentWeight","Height","goalWeight","Age","Phone","Address"};
+        String selection = "employeeId=?";
+        String[] selectionArgs = {employeeId};
 
-        long result = DB.update("StudentData", contentV, "studentId = ?", new String[]{studentId});
-        DB.close();
-
-        return result != -1;
-
-    }
-
-    public boolean DeleteData(String studentId) {
-        SQLiteDatabase DB = this.getWritableDatabase();
-
-        long result = DB.delete("StudentData", "studentId=?", new String[]{studentId});
-        DB.close();
-        return result != -1; // Return true if the deletion was successful
-    }
-
-    public Cursor SearchData(String studentId) {
-        SQLiteDatabase DB = this.getWritableDatabase();
-        String[] columns = {"studentId", "studentName", "studentCourse"};
-        String selection = "studentId=?";
-        String[] selectionArgs = {studentId};
-
-        Cursor cursor = DB.query("StudentData", columns, selection, selectionArgs, null, null, null);
+        Cursor cursor = DB.query("EmployeesTable", columns, selection, selectionArgs, null, null, null);
         return cursor;
     }
 
--- Basics/FinalsAct1/app/src/main/java/com/example/finalsact1/MainActivity.java
+++ Basics/FinalsAct1/app/src/main/java/com/example/finalsact1/MainActivity.java
@@ -13,14 +13,17 @@ import android.provider.AlarmClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
     public class MainActivity extends AppCompatActivity {
 
-        String _studentId,_studentName,_studentCourse;
-        EditText studentId, studentName, studentCourse;
-        Button search, add, update,delete,close;
+        EditText employeeId, employeeFullName,employeeCurrentWeight,
+                employeeHeight, employeeGoalWeight, employeeAge, employeePhoneNum, employeeAddress;
+        Button search,submit;
 
+        //GENDER
+        RadioButton Male,Female,Others;
         DBHelper dbContext;
 
 
@@ -29,54 +32,53 @@ import android.widget.Toast;
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
 
-            //TEXT VIEWS
-            studentId = (EditText) findViewById(R.id.txt_studentNumber);
-            studentName = (EditText) findViewById(R.id.txt_enterName);
-            studentCourse = (EditText) findViewById(R.id.txt_enterCourse);
+            //EDIT TEXT
+            employeeId = (EditText) findViewById(R.id.txt_employeeId);
+            employeeFullName = (EditText) findViewById(R.id.txt_enterFullName);
+            employeeCurrentWeight = (EditText) findViewById(R.id.txt_currentWeight);
+            employeeHeight = (EditText) findViewById(R.id.txt_Height);
+            employeeGoalWeight = (EditText) findViewById(R.id.txt_goalWeight);
+            employeeAge = (EditText) findViewById(R.id.txt_Age);
+            employeePhoneNum = (EditText) findViewById(R.id.txt_Phone);
+            employeeAddress = (EditText) findViewById(R.id.txt_Address);
+
+            //RADIO BUTTON
+            Male = (RadioButton) findViewById(R.id.rbtn_Male);
+            Female = (RadioButton) findViewById(R.id.rbtn_Female);
+            Others = (RadioButton) findViewById(R.id.rbtn_Other);
 
             //BUTTON
-            search = (Button) findViewById(R.id.btn_searchStudentNumber);
-            add = (Button) findViewById(R.id.btn_save);
-            update = (Button) findViewById(R.id.btn_update);
-            delete = (Button)findViewById(R.id.btn_delete);
-            close = (Button) findViewById(R.id.btn_close);
+            search = (Button) findViewById(R.id.btn_searchEmployeeId);
+            submit = (Button) findViewById(R.id.btn_submit);
 
-            //DataBase
+            //DATABASE
             dbContext = new DBHelper(this);
 
-            search.setOnClickListener(new View.OnClickListener() {
+            submit.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    String studentIdTxt = studentId.getText().toString(); // Student ID
-                    Cursor result = dbContext.SearchData(studentIdTxt);
+                    String employeeIdTxt = employeeId.getText().toString();
+                    String employeeFullNameTxt = employeeFullName.getText().toString();
+                    String employeeCurrentWeightTxt = employeeCurrentWeight.getText().toString();
+                    String employeeHeightTxt = employeeHeight.getText().toString();
+                    String employeeGoalWeightTxt = employeeGoalWeight.getText().toString();
+                    String employeeAgeTxt = employeeAge.getText().toString();
+                    String employeePhoneNumTxt = employeePhoneNum.getText().toString();
+                    String employeeAddressTxt = employeeAddress.getText().toString();
 
-                    if (result.getCount() == 0) {
-                        Toast.makeText(MainActivity.this, "Data Not Found", Toast.LENGTH_SHORT).show();
-                    } else {
-                        StringBuffer buffer = new StringBuffer();
-                        while (result.moveToNext()) {
-                            buffer.append("Student ID: " + result.getString(0) + "\n");
-                            buffer.append("Student Name: " + result.getString(1) + "\n");
-                            buffer.append("Student Course: " + result.getString(2) + "\n");
-                        }
+                    String gender = ""; // Initialize gender as an empty string
 
-                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                        builder.setCancelable(true);
-                        builder.setTitle("Student Data");
-                        builder.setMessage(buffer.toString());
-                        builder.show();
+                    if (Male.isChecked()) {
+                        gender = "Male";
+                    } else if (Female.isChecked()) {
+                        gender = "Female";
+                    } else if (Others.isChecked()) {
+                        gender = "Others";
                     }
-                }
-            });
 
-            add.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    String studentIdTxt = studentId.getText().toString();
-                    String studentNameTxt = studentName.getText().toString();
-                    String studentCourseTxt = studentCourse.getText().toString();
+                    // Now, you can insert this data into the database
+                    Boolean checkInsertData = dbContext.InsertData(employeeIdTxt, employeeFullNameTxt, gender, employeeCurrentWeightTxt, employeeHeightTxt, employeeGoalWeightTxt, employeeAgeTxt, employeePhoneNumTxt, employeeAddressTxt);
 
-                    Boolean checkInsertData = dbContext.InsertData(studentIdTxt,studentNameTxt,studentCourseTxt);
 
                     if(checkInsertData == true)
                     {
@@ -86,63 +88,44 @@ import android.widget.Toast;
                     {
                         Toast.makeText(MainActivity.this, "Failed to Insert Data",Toast.LENGTH_SHORT).show();
                     }
+
                 }
             });
 
-        update.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String studentIdTxt = studentId.getText().toString(); // Student ID
-                String newStudentNameTxt = studentName.getText().toString(); // New student name
-                String newStudentCourseTxt = studentCourse.getText().toString(); // New student course
-
-                Boolean checkUpdateData = dbContext.UpdateData(studentIdTxt, newStudentNameTxt, newStudentCourseTxt);
-
-                if (checkUpdateData) {
-                    Toast.makeText(MainActivity.this, "Data Updated Successfully", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(MainActivity.this, "Failed to Update Data", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        delete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String studentIdTxt = studentId.getText().toString(); // Student ID
-                Boolean checkDeleteData = dbContext.DeleteData(studentIdTxt);
-
-                if(checkDeleteData == true)
-                {
-                    Toast.makeText(MainActivity.this, "Data Deleted Successfully",Toast.LENGTH_SHORT).show();
-                }
-                else
-                {
-                    Toast.makeText(MainActivity.this, "Failed to Delete Data",Toast.LENGTH_SHORT).show();
-
-                }
-            }
-        });
-
-            close.setOnClickListener(new View.OnClickListener() {
+            search.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick(View view) {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                    builder.setMessage("Are you sure you want to exit?").
-                            setCancelable(false);
-                    builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            MainActivity.this.finish(); }
-                    });
+                public void onClick(View v) {
+                    String employeeIdTxt = employeeId.getText().toString(); // Student ID
+                    Cursor result = dbContext.SearchData(employeeIdTxt);
 
-                    builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            dialog.cancel();     }
-                    });
-                    AlertDialog alert = builder.create();
-                    alert.show();
+                    if (result.getCount() == 0) {
+                        Toast.makeText(MainActivity.this, "Data Not Found", Toast.LENGTH_SHORT).show();
+                    } else {
+                        StringBuffer buffer = new StringBuffer();
+                        while (result.moveToNext()) {
+                            buffer.append("Employee ID: " + result.getString(0) + "\n");
+                            buffer.append("Employee Full Name: " + result.getString(1) + "\n");
+                            buffer.append("Employee Gender: " + result.getString(2) + "\n");
+                            buffer.append("Employee Current Weight: " + result.getString(3) + "\n");
+                            buffer.append("Employee Height: " + result.getString(4) + "\n");
+                            buffer.append("Employee Goal Weight: " + result.getString(5) + "\n");
+                            buffer.append("Employee Age: " + result.getString(6) + "\n");
+                            buffer.append("Employee Phone #: " + result.getString(7) + "\n");
+                            buffer.append("Employee Address: " + result.getString(8) + "\n");
+                        }
+
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setCancelable(true);
+                        builder.setTitle("Employee Data");
+                        builder.setMessage(buffer.toString());
+                        builder.show();
+                    }
                 }
             });
 
-    }
+
+
+
+
+        }
 }
